88b0b9694e131a2b3a3a991c5faac7156956e305
Trever Andersen <trever.r.andersen@gmail.com>
2017-10-20 02:26:37 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,4 +6,11 @@ class Song
     @name = name
   end
 
+  def self.new_by_filename(file)
+    artist_name, song_name = file.split(" - ")
+    new_song = Song.new(song_name)
+    new_song.artist = Artist.find_or_create_by_name(artist_name)
+    new_song
+  end
+  
 end
