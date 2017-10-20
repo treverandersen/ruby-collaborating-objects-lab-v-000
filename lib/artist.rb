1d465549fedac0a21a1e2cda57a1037af6ca088a
Trever Andersen <trever.r.andersen@gmail.com>
2017-10-20 00:50:30 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,14 +1,15 @@
 class Artist
-  attr_accessor :name
+  attr_accessor :name, :songs
 
   @@all = []
-
+  
   def initialize(name)
     @name = name
+    @songs = []
   end
 
   def add_song(song)
-    @@all << song
+    @songs << song
   end
 
 end
